b697c17bba58dfd388ae025b813e0cfba1d164fe
saintkreavi <119168743+iLuxyy@users.noreply.github.com>
2026-07-21 07:37:37 +0700

    Add files via upload

---
+++ dataucapan.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="stylesheet" href="css/style.css" />    
+    <link rel="stylesheet" href="countdown/simplyCountdown.theme.default.css"/>
+    <script src="countdown/simplyCountdown.min.js"></script>
+    <title>Invitation Kiki&Sanjay</title>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Waterfall&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Stalemate&display=swap" rel="stylesheet">
+
+</head>
+<body>           
+    <?php
+        include "service/database.php";
+        
+        $ambildata = mysqli_query($db, "SELECT * FROM tb_undangan");
+        while ($tampil = mysqli_fetch_array($ambildata)){
+            echo "
+                <tr>
+                    <div class='chat-bubble sent'>
+                        <p>tes<b> $tampil[nama]</b><br>
+                        di <i>$tampil[alamat]</i><br>
+                        $tampil[ucapan]</p>
+                        </div>
+                </tr>
+            ";
+        }
+    ?>                                
+</body>
+</html>
